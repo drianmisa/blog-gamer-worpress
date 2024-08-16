b1b69576e0590e7f32e0d95a47811041ed8753a1
drianmisa <adrian.islas.lopez@gmail.com>
2024-08-15 21:02:20 -0600

    Ajustes de single post

--- wp-content/themes/blog-gamer/comp/footer/footer.php
+++ wp-content/themes/blog-gamer/comp/footer/footer.php
@@ -1,55 +1,17 @@
 <footer class="wrap">
   <div class="comp-footer">
     <div class="item-wrap">
-      <div class="btn-hdr">
-        <a class="btn-rosa" href="http://">Editor's pick</a>
-        <h3>The Art of Storytelling</h3>
+      <div class="logo">
+        <?php
+        if (has_custom_logo()) {
+          the_custom_logo();
+        } else {
+          echo '<a class="title" href="' . esc_url(home_url('/')) . '">' . get_bloginfo('name') . '</a>';
+        }
+        ?>
       </div>
-      <ul class="footer-list">
-        <li>
-          <a href="">This game raises the bar for excellence in storytelling, gameplay, and overall enjoyment.</a>
-        </li>
-        <li>
-          <a href="">Gamer’s Delight: Top Picks from [Magazine Name]’s Latest Edition</a>
-        </li>
-        <li>
-          <a href="">Leveling Up: The Evolution of [Magazine Name] and Gaming Journalism</a>
-        </li>
-      </ul>
-    </div>
-    <div class="item-wrap">
-      <?php
-      if (has_custom_logo()) {
-        the_custom_logo();
-      } else {
-        echo '<a class="title" href="' . esc_url(home_url('/')) . '">' . get_bloginfo('name') . '</a>';
-      }
-      ?>
-      <p>This theme is perfect for blogs and excellent for online stores, news, magazine or review sites. Buy Soledad now!</p>
+      <p>Descubre un mundo de ideas en nuestro blog: desde tendencias y consejos hasta historias inspiradoras. ¡Explora y encuentra lo que te mueve!</p>
       <?php include(get_template_directory() . "/comp/header/social.php"); ?>
-    </div>
-    <div class="item-wrap">
-      <h3>
-        USERFUL LINKS
-      </h3>
-      <ul>
-        <li>
-          <a href="http://">Enlace 1</a>
-        </li>
-        <li>
-          <a href="http://">Enlace 1</a>
-        </li>
-        <li>
-          <a href="http://">Enlace 1</a>
-        </li>
-        <li>
-          <a href="http://">Enlace 1</a>
-        </li>
-        <li>
-          <a href="http://">Enlace 1</a>
-        </li>
-      </ul>
-
     </div>
   </div>
 </footer>
